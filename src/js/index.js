fd95043fcafd3b834b6309382e423b4f52b7e8a6
zyddnys <zyddnys@outlook.com>
2020-08-08 22:31:18 -0400

    add debug

--- src/js/index.js
+++ src/js/index.js
@@ -3,5 +3,6 @@ import DPlayer from './player';
 
 /* global DPLAYER_VERSION GIT_HASH */
 console.log(`${'\n'} %c DPlayer v${DPLAYER_VERSION} ${GIT_HASH} %c http://dplayer.js.org modified by https://github.com/zyddnys ${'\n'}${'\n'}`, 'color: #fadfa3; background: #030307; padding:5px 0;', 'background: #fadfa3; padding:5px 0;');
+console.log("test test test");
 
 export default DPlayer;
