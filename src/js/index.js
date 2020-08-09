2532e8bec810f604f650cd9420c79b0c14dadb43
zyddnys <zyddnys@outlook.com>
2020-08-08 22:32:57 -0400

    remove welcome info

--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,7 @@ import '../css/index.scss';
 import DPlayer from './player';
 
 /* global DPLAYER_VERSION GIT_HASH */
-console.log(`${'\n'} %c DPlayer v${DPLAYER_VERSION} ${GIT_HASH} %c http://dplayer.js.org modified by https://github.com/zyddnys ${'\n'}${'\n'}`, 'color: #fadfa3; background: #030307; padding:5px 0;', 'background: #fadfa3; padding:5px 0;');
-console.log("test test test");
+//console.log(`${'\n'} %c DPlayer v${DPLAYER_VERSION} ${GIT_HASH} %c http://dplayer.js.org modified by https://github.com/zyddnys ${'\n'}${'\n'}`, 'color: #fadfa3; background: #030307; padding:5px 0;', 'background: #fadfa3; padding:5px 0;');
+//console.log("test test test");
 
 export default DPlayer;
